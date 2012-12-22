5d69dffd3e11a59f5500c63f6819bd88094e8508
Julio Raffaine <julio.loxas@gmail.com>
2012-12-21 23:35:48 -0200

    Uma versao que ja da pra testar o server e ilustrar condicoes nao resolvidas, como alto spawn rate dos clientes e o servidor se embanando com a criacao dos processos que cuidam das mesas

--- AgentLoadTest.py
+++ AgentLoadTest.py
@@ -5,23 +5,28 @@ import subprocess
 HALL_PROG = 'kingHallAgent.py'
 CLIENT = 'kingClient.py'
 
-NUM_CLIENTS = 8
+NUM_CLIENTS = 4
+EXEC = sys.executable if sys.platform != 'win32' else "python.exe"
+
+SPAWN_RATE = 10 #PER MINUTE
 
 #logHall = open("logHall.log","w")
 #hall = subprocess.Popen([sys.executable, HALL_PROG], stdout=logHall)
+print "Starting Hall Agent"
+hall = subprocess.Popen([EXEC, HALL_PROG])
 
 clients = []
 for c in range(NUM_CLIENTS):
-  log_c = open("logC%d.log"%c,"w")
-  clients.append(subprocess.Popen([sys.executable,
-                                   CLIENT, str(c)], stdout=log_c))
+  #log_c = open("logC%d.log"%c,"w")
+  print "Starting Client %s"%c
+  clients.append(subprocess.Popen([EXEC, CLIENT, str(c)]))
+  time.sleep(60/SPAWN_RATE)
 
 try:
-  while filter(lambda x: not x.poll(), clients):
-    time.sleep(10)
+  while filter(lambda x: x.poll() == None, clients):
+    time.sleep(5)  
 except KeyboardInterrupt:
-  pass
+  for c in clients:
+    c.terminate()
 
-for c in clients:
-  c.terminate()
-#hall.terminate()
+hall.terminate()
