615dd0e02f04371a25eeca74b0ba31ca2a58a3d8
Julio Raffaine <julio.loxas@gmail.com>
2016-12-25 19:55:49 -0800

    Implement console UI for client so humans can play

--- human.py
+++ human.py
@@ -1,17 +1,67 @@
 #!/usr/bin/env python3
-
+"""
+    Console client for the King Game
+"""
 import sys
+import os
+import time
 import client
 
 USR_NAME = ''
+CLR_CMD = 'cls' if sys.platform == 'win32' else 'clear'
+
+def clear_screen():
+    _ = os.system(CLR_CMD)
+
+def print_hand(player):
+    print("Your Hand:", player.game.hand, "\n")
 
 class HumanPlayer(client.GamePlayer):
     """ Implements a Console based human player """
     def choose_game(self, choices):
-        pass
+        clear_screen()
+        print("A new hand is about to start, you must choose a game.")
+        print_hand(self)
+        print("Your choices:", choices, "\n")
+        choice = ''
+        while not choice:
+            res = input('>')
+            choice = res if res in choices else ''
+
+        return choice
+
+    def game_selected(self):
+        clear_screen()
+        print("A new hand started. The game will be", self.game.game)
+        time.sleep(2)
 
     def play_card(self):
-        pass
+        clear_screen()
+        print("It's your turn, choose card. (Game is %s)\n"%(self.game.game))
+        print_hand(self)
+        print("Table is:", self.game.table)
+        card = ''
+        while not card:
+            res = input('>')
+            card = res if res in self.game.hand else ''
+
+        return card
+
+    def card_played(self):
+        name = self.game.turn if self.game.turn != USR_NAME else 'You'
+        print(name, "played", self.game.table[-1])
+        print("Table now is: ", self.game.table)
+        time.sleep(2)
+
+    def end_round(self, winner):
+        name, verb = (winner, "takes") if winner != USR_NAME else ('You', 'took')
+        print(name, verb, "the round")
+        time.sleep(4)
+        clear_screen()
+
+    def end_hand(self):
+        time.sleep(1)
+        clear_screen()
 
 def choose_table(server):
     """ Allows user to choose a game of his choice:
